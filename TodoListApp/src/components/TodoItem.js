5470ffbef9a3d4f8c65be142a4bc22c36d715261
unknown <abusayedahmadruet13@gmail.com>
2020-03-21 02:38:25 -0700

    Todo List App beginning tutorial based app

---
+++ TodoListApp/src/components/TodoItem.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class TodoItem extends Component {
+    getStyle=()=>{
+        return{
+            
+            textDecoration: this.props.todo.completed? 'line-through':'none',
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted'
+        
+        }        
+    }
+    
+    render() {
+        const{ id, title}=this.props.todo;
+        return (
+            <div style={this.getStyle()}>                
+                <p>
+                    <input type='checkbox' onChange={this.props.markComplete.bind(this,id)}/>{' '}
+                    {title}
+                    <button onClick={this.props.delTodo.bind(this,id)} style={btnStyle}>x</button>
+                    
+                </p>
+            </div>
+        )
+    }
+}
+
+TodoItem.propTypes={
+    todo: PropTypes.object.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
+}
+
+const btnStyle={
+    background: '#ff0000',
+    color: 'fff',
+    border: 'none',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    padding: '5px 10px',
+    float: 'right'
+    
+}
+
+export default TodoItem
---
+++ TodoListApp/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a Todo List App using React js to Learn React</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
